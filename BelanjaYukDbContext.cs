0cdb1158b8af8cfea9fef9c71d18e040fd4894b1
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-30 10:43:43 +0700

    add entity relationships with fluent api for database model configuration

--- BelanjaYukDbContext.cs
+++ BelanjaYukDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using BelanjaYuk.API.Models;
 
 namespace BelanjaYuk.API.Data
 {
@@ -15,7 +16,7 @@ namespace BelanjaYuk.API.Data
         public DbSet<MsUserSeller> MsUserSellers { get; set; }
         public DbSet<MsProduct> MsProducts { get; set; }
         public DbSet<LtCategory> LtCategories { get; set; }
-        public DbSet<TrProductImage> TrProductImages { get; set; } // ERD: TrProductimages
+        public DbSet<TrProductImage> TrProductImages { get; set; }
         public DbSet<TrBuyerCart> TrBuyerCarts { get; set; }
         public DbSet<LtPayment> LtPayments { get; set; }
         public DbSet<TrBuyerTransaction> TrBuyerTransactions { get; set; }
@@ -25,38 +26,83 @@ namespace BelanjaYuk.API.Data
         {
             base.OnModelCreating(modelBuilder);
             
+            // Relation one-to-one
             modelBuilder.Entity<MsUser>()
                 .HasOne(user => user.SellerInfo)
                 .WithOne(seller => seller.User)
                 .HasForeignKey<MsUserSeller>(seller => seller.IdUser);
 
+            // Relation one-to-Many
             modelBuilder.Entity<MsUserSeller>()
                 .HasMany(seller => seller.Products)
                 .WithOne(product => product.Seller)
                 .HasForeignKey(product => product.IdUserSeller);
 
-             modelBuilder.Entity<LtCategory>()
+            // Relation one-to-Many
+            modelBuilder.Entity<LtCategory>()
                 .HasMany(category => category.Products)
                 .WithOne(product => product.Category)
                 .HasForeignKey(product => product.IdCategory);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsUser>()
+                .HasMany(user => user.Passwords)
+                .WithOne(pass => pass.User)
+                .HasForeignKey(pass => pass.IdUser);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsUser>()
+                .HasMany(user => user.HomeAddresses)
+                .WithOne(addr => addr.User)
+                .HasForeignKey(addr => addr.IdUser);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsUser>()
+                .HasMany(user => user.BuyerCarts)
+                .WithOne(cart => cart.User)
+                .HasForeignKey(cart => cart.IdUser);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsUser>()
+                .HasMany(user => user.BuyerTransactions)
+                .WithOne(trans => trans.User)
+                .HasForeignKey(trans => trans.IdUser);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<LtGender>()
+                .HasMany(gender => gender.Users)
+                .WithOne(user => user.Gender)
+                .HasForeignKey(user => user.IdGender);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsProduct>()
+                .HasMany(prod => prod.ProductImages)
+                .WithOne(img => img.Product)
+                .HasForeignKey(img => img.IdProduct);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsProduct>()
+                .HasMany(prod => prod.BuyerCarts)
+                .WithOne(cart => cart.Product)
+                .HasForeignKey(cart => cart.IdProduct);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<MsProduct>()
+                .HasMany(prod => prod.TransactionDetails)
+                .WithOne(detail => detail.Product)
+                .HasForeignKey(detail => detail.IdProduct);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<LtPayment>()
+                .HasMany(pay => pay.BuyerTransactions)
+                .WithOne(trans => trans.Payment)
+                .HasForeignKey(trans => trans.IdPayment);
+
+            // Relation one-to-Many
+            modelBuilder.Entity<TrBuyerTransaction>()
+                .HasMany(trans => trans.TransactionDetails)
+                .WithOne(detail => detail.BuyerTransaction)
+                .HasForeignKey(detail => detail.IdBuyerTransaction);
         }
     }
-
-    public class MsUser {}
-    public class MsUserPassword {}
-    public class LtGender {}
-    public class TrHomeAddress {}
-    public class MsUserSeller { 
-    }
-    public class MsProduct { 
-    }
-    public class LtCategory { 
-    }
-    
-    public class TrProductImage {}
-    public class TrBuyerCart {}
-    public class LtPayment {}
-    public class TrBuyerTransaction {}
-    public class TrBuyerTransactionDetail {}
-
 }
